7686bf8ab8ec445d0c4a5123ba7a462d673b1471
liannes <liannes@google.com>
2018-09-04 16:03:13 -0700

    Fixed instance of tuple array.

--- app/src/main/java/com/techexhange/mobileapps/assignment1/MainActivity.java
+++ app/src/main/java/com/techexhange/mobileapps/assignment1/MainActivity.java
@@ -47,57 +47,46 @@ public class MainActivity extends AppCompatActivity {
 
         one =findViewById(R.id.one_image);
         one.setOnClickListener(this::onImagePressed);
-        one.setImageBitmap(sprite);
         imagesViewGroup[1]= new Tuple(one,1,sprite);
         //imagesGroup[1]=one;
 
-
-
         two =findViewById(R.id.two_image);
         two.setOnClickListener(this::onImagePressed);
-        two.setImageBitmap(sprite2);
         imagesViewGroup[2]= new Tuple(two,2,sprite2);
         //imagesGroup[2]=two;
 
         three =findViewById(R.id.three_image);
         three.setOnClickListener(this::onImagePressed);
-        three.setImageBitmap(sprite3);
         imagesViewGroup[3]= new Tuple(three,3,sprite3);
         //imagesGroup[3]=three;
 
         four = findViewById(R.id.four_image);
         four.setOnClickListener(this::onImagePressed);
-        four.setImageBitmap(sprite4);
         imagesViewGroup[4]= new Tuple(four,4,sprite4);
        // imagesGroup[4]=four;
 
         five = findViewById(R.id.five_image);
         five.setOnClickListener(this::onImagePressed);
-        five.setImageBitmap(sprite5);
         imagesViewGroup[5]= new Tuple(five,5,sprite5);
         //imagesGroup[5]=five;
 
         six = findViewById(R.id.six_image);
         six.setOnClickListener(this::onImagePressed);
-        six.setImageBitmap(sprite6);
         imagesViewGroup[6]= new Tuple(six,6,sprite6);
         //imagesGroup[6]=six;
 
         seven = findViewById(R.id.seven_image);
         seven.setOnClickListener(this::onImagePressed);
-        seven.setImageBitmap(sprite7);
         imagesViewGroup[7]= new Tuple(seven,7,sprite7);
        // imagesGroup[7]=seven;
 
         eight = findViewById(R.id.eight_image);
         eight.setOnClickListener(this::onImagePressed);
-        eight.setImageBitmap(sprite8);
         imagesViewGroup[8]= new Tuple(eight,8,sprite8);
      //   imagesGroup[8]=eight;
 
         zero = findViewById(R.id.zero_image);
         zero.setOnClickListener(this::onImagePressed);
-        zero.setImageBitmap(sprite0);
         //zero.setVisibility(View.INVISIBLE);
         imagesViewGroup[0]= new Tuple(zero,0,sprite0);
         //imagesGroup[0]=zero;
@@ -119,21 +108,33 @@ public class MainActivity extends AppCompatActivity {
 
     private void onImagePressed(View v){
 
-        int Testing = coordinatesMap.get(v);
+        int oldPosition = coordinatesMap.get(v);
+        System.out.println("POSITION:  " + oldPosition);
         int row = (coordinatesMap.get(v))/3;
         int column = (coordinatesMap.get(v))%3;
-        System.out.println("Testing: "+ Testing+" Row: "+row+"Column"+column);
+        System.out.println("Testing: "+ oldPosition+" Row: "+row+"Column"+column);
         boolean[] directions = game.swapTile(column,row);
         int newPosition = this.getCoordinate(coordinatesMap.get(v),directions); //Int position to move
         if (newPosition<9){
-            Bitmap oldBitmap = inverseCoordinates.get(coordinatesMap.get(v)).getBitmap();
-            inverseCoordinates.get(newPosition).setBitmap(oldBitmap);
-            ImageView toMove = inverseCoordinates.get(coordinatesMap.get(v)).getImage(); //The number of the image
-            ImageView NewImage = inverseCoordinates.get(newPosition).getImage();
-            NewImage.setImageBitmap(inverseCoordinates.get(coordinatesMap.get(toMove)).getBitmap());
 
-            toMove.setVisibility(View.INVISIBLE);
-            NewImage.setVisibility(View.VISIBLE);
+            //ImageView oldImage = inverseCoordinates.get(coordinatesMap.get(v)).getImage();
+
+            coordinatesMap.replace((ImageView) v,newPosition);
+            coordinatesMap.replace(inverseCoordinates.get(newPosition).getImage(), oldPosition);
+
+
+
+
+
+
+            //Bitmap oldBitmap = inverseCoordinates.get(coordinatesMap.get(v)).getBitmap();
+            //inverseCoordinates.get(newPosition).setBitmap(oldBitmap);
+            //ImageView toMove = inverseCoordinates.get(coordinatesMap.get(v)).getImage(); //The number of the image
+            //ImageView NewImage = inverseCoordinates.get(newPosition).getImage();
+            //NewImage.setImageBitmap(inverseCoordinates.get(coordinatesMap.get(toMove)).getBitmap());
+
+            //toMove.setVisibility(View.INVISIBLE);
+            //NewImage.setVisibility(View.VISIBLE);
         }
         //Boolean wonStatus = game.checkIfWon();
         //if(wonStatus) this.won();
@@ -214,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
                 bases = res.getIntArray(R.array.game_5);
                 break;
         }
+        Tuple[] order = new Tuple[9];
         int k=0;
         for(int i=0;i<NumSlider.winningNUmbers.length;i++){
             for(int j=0;j<NumSlider.winningNUmbers.length;j++){
@@ -226,33 +228,21 @@ public class MainActivity extends AppCompatActivity {
                     NumSlider.gridTiles[i][j].setTileStatus(false);
                 }
                 NumSlider.gridTiles[i][j].setTileValue(bases[k]);
+                //coordinatesMap.replace(imagesViewGroup[bases[k]].getImage(), k);
+                //imagesViewGroup[bases[k]].setValue(k);
+                order[k] = imagesViewGroup[bases[k]];
+                order[k].getImage().setImageBitmap(order[k].getBitmap());
 
-                //int oldPosition = coordinatesMap.get(imagesViewGroup[bases[k]].getValue());
-                coordinatesMap.replace(imagesViewGroup[bases[k]].getImage(), k);
-                imagesViewGroup[bases[k]].setValue(k);
-                inverseCoordinates.replace(k,imagesViewGroup[bases[k]]);
-               // coordinatesMap.replace(imagesGroup[k],oldPosition);
-                //Tuple tempTuple = inverseCoordinates.get(k);
-
-
-               // inverseCoordinates.replace(k,inverseCoordinates.get(bases[k]));
-                //inverseCoordinates.replace(bases[k],tempTuple);
-
+              //
+                //
+                // inverseCoordinates.replace(k,imagesViewGroup[bases[k]]);
 
                 k=k+1;
             }
         }
 
-        for (int i=0;i<coordinatesMap.size();i++){
-            Tuple tempTesting = inverseCoordinates.get(i);
-            int testint = coordinatesMap.get(inverseCoordinates.get(i).getImage());
-            //System.out.print(inverseCoordinates.get(i));
-        }
-//        for(int i=0;i<NumSlider.gridTiles.length;i++){
-//            for(int j=0;j<NumSlider.gridTiles.length;j++){
-//                System.out.println("Inside Tiles: row: "+ i + " Column: " + j+ " Value "+ NumSlider.gridTiles[i][j].getTileValue()+ " Valid : " +NumSlider.gridTiles[i][j].getTileStatus());
-//            }
-//        }
+
+
 
     }
 
