e398167e397cb540a37ea28a3ca5fbdd65652ebd
liannes <liannes@google.com>
2018-09-05 10:48:11 -0700

    Commenting in the class Tile.

--- app/src/main/java/com/techexhange/mobileapps/assignment1/MainActivity.java
+++ app/src/main/java/com/techexhange/mobileapps/assignment1/MainActivity.java
@@ -7,11 +7,8 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 
@@ -19,11 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private NumSlider game;
     private ImageView one,two,three,four,five,six,seven,eight,zero;
-   // private Map<ImageView, Integer> coordinatesMap;
-    //private Map<Integer, Tuple> inverseCoordinates;
     private Bitmap spritesheet;
     private ImageView[] imagesGroup = new ImageView[9];
-    //private Tuple[] imagesViewGroup = new Tuple[9];
     private Bitmap[] sprites = new Bitmap[9];
     boolean[] directions = new boolean[4];
 
@@ -44,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
         Bitmap sprite8 = Bitmap.createBitmap(spritesheet,2400,0,300,300);sprites[8]=sprite8;
 
         game = new NumSlider();
-       // coordinatesMap = new HashMap<ImageView, Integer>();
-       // inverseCoordinates = new HashMap<Integer, Tuple>();
 
         one =findViewById(R.id.one_image);
         one.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 1, 0);
             }
         });
-        //imagesViewGroup[1]= new Tuple(one,1,sprite);
         imagesGroup[1]=one;
 
         two =findViewById(R.id.two_image);
@@ -66,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 2, 0);
             }
         });
-        //imagesViewGroup[2]= new Tuple(two,2,sprite2);
         imagesGroup[2]=two;
 
         three =findViewById(R.id.three_image);
@@ -77,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 0, 1);
             }
         });
-        //imagesViewGroup[3]= new Tuple(three,3,sprite3);
         imagesGroup[3]=three;
 
         four = findViewById(R.id.four_image);
@@ -88,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 1, 1);
             }
         });
-       // imagesViewGroup[4]= new Tuple(four,4,sprite4);
         imagesGroup[4]=four;
 
         five = findViewById(R.id.five_image);
@@ -99,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 2, 1);
             }
         });
-        //imagesViewGroup[5]= new Tuple(five,5,sprite5);
         imagesGroup[5]=five;
 
         six = findViewById(R.id.six_image);
@@ -110,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 0, 2);
             }
         });
-        //imagesViewGroup[6]= new Tuple(six,6,sprite6);
         imagesGroup[6]=six;
 
         seven = findViewById(R.id.seven_image);
@@ -121,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 1, 2);
             }
         });
-        //imagesViewGroup[7]= new Tuple(seven,7,sprite7);
         imagesGroup[7]=seven;
 
         eight = findViewById(R.id.eight_image);
@@ -132,7 +117,6 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 2, 2);
             }
         });
-        //imagesViewGroup[8]= new Tuple(eight,8,sprite8);
         imagesGroup[8]=eight;
 
         zero = findViewById(R.id.zero_image);
@@ -143,76 +127,22 @@ public class MainActivity extends AppCompatActivity {
                 swappingImages(directions, 0, 0);
             }
         });
-        //zero.setVisibility(View.INVISIBLE);
-       // imagesViewGroup[0]= new Tuple(zero,0,sprite0);
         imagesGroup[0]=zero;
-
-
-
-
-
-        //coordinatesMap.put(one,1); inverseCoordinates.put(1,new Tuple(one,1,sprite));
-        //coordinatesMap.put(two,2); inverseCoordinates.put(2,new Tuple(two,2,sprite2));
-        //coordinatesMap.put(three,3); inverseCoordinates.put(3,new Tuple(three,3,sprite3));
-        //coordinatesMap.put(four,4); inverseCoordinates.put(4,new Tuple(four,4,sprite4));
-        //coordinatesMap.put(five,5); inverseCoordinates.put(5,new Tuple(five,5,sprite5));
-        //coordinatesMap.put(six,6); inverseCoordinates.put(6, new Tuple(six,6,sprite6));
-        //coordinatesMap.put(seven,7); inverseCoordinates.put(7, new Tuple(seven,7,sprite7));
-        //coordinatesMap.put(eight,8); inverseCoordinates.put(8,new Tuple(eight,8,sprite8));
-        //coordinatesMap.put(zero,0); inverseCoordinates.put(0,new Tuple(zero,0,sprite0));
         loadGames();
     }
 
 
     private void swappingImages(boolean[] directions, int column, int row ){
         int oldPosition = (row*3) + column;
-        System.out.println("Old Position: "+ oldPosition);
-
         int newPosition = this.getCoordinate(oldPosition, directions);
-        System.out.println("New Position: "+ newPosition);
         if(newPosition<9){
-            //imagesGroup[newPosition].setImageBitmap(sprites[oldPosition]);
             imagesGroup[newPosition].setVisibility(View.VISIBLE);
-           // imagesGroup[newPosition].setImageDrawable(null);
-            System.out.println("GRIDDDDD: "+ NumSlider.gridTiles[row][column].getTileValue());
             imagesGroup[newPosition].setImageBitmap(sprites[ NumSlider.gridTiles[newPosition/3][newPosition%3].getTileValue()]);
             imagesGroup[oldPosition].setVisibility(View.INVISIBLE);
-            //imagesGroup[newPosition].setVisibility(View.INVISIBLE)
-
         }
         Boolean wonStatus = game.checkIfWon();
         if(wonStatus) this.won();
     }
-    //private void onImagePressed(View v, int index){
-
-        //--int oldPosition = coordinatesMap.get(v);
-        //--System.out.println("POSITION:  " + oldPosition);
-       //-- int row = (coordinatesMap.get(v))/3;
-       //-- int column = (coordinatesMap.get(v))%3;
-       //-- System.out.println("Testing: "+ oldPosition+" Row: "+row+"Column"+column);
-       //-- boolean[] directions = game.swapTile(column,row);
-       //int newPosition = this.getCoordinate(coordinatesMap.get(v),directions); //Int position to move
-       //-- if (newPosition<9){
-
-            //ImageView oldImage = inverseCoordinates.get(coordinatesMap.get(v)).getImage();
-
-        //--    coordinatesMap.replace((ImageView) v,newPosition);
-        //--    coordinatesMap.replace(inverseCoordinates.get(newPosition).getImage(), oldPosition);
-
-
-            //Bitmap oldBitmap = inverseCoordinates.get(coordinatesMap.get(v)).getBitmap();
-            //inverseCoordinates.get(newPosition).setBitmap(oldBitmap);
-            //ImageView toMove = inverseCoordinates.get(coordinatesMap.get(v)).getImage(); //The number of the image
-            //ImageView NewImage = inverseCoordinates.get(newPosition).getImage();
-            //NewImage.setImageBitmap(inverseCoordinates.get(coordinatesMap.get(toMove)).getBitmap());
-
-            //toMove.setVisibility(View.INVISIBLE);
-            //NewImage.setVisibility(View.VISIBLE);
-      //  }
-        //Boolean wonStatus = game.checkIfWon();
-        //if(wonStatus) this.won();
-        //return v;
-   // }
 
     private int getCoordinate(int number, boolean[] directions){
         int result=9;
@@ -238,8 +168,6 @@ public class MainActivity extends AppCompatActivity {
     private void won(){
         changeBitMapsColor();
         Toast.makeText(MainActivity.this, "CONGRATS YOU WON!", Toast.LENGTH_SHORT).show();
-
-
     }
 
     private void changeBitMapsColor(){
@@ -300,25 +228,12 @@ public class MainActivity extends AppCompatActivity {
             for(int j=0;j<NumSlider.winningNUmbers.length;j++){
                 if(bases[k]==0) {
                     NumSlider.gridTiles[i][j].setTileStatus(true);
-                   // imagesGroup[k].setVisibility(View.INVISIBLE);
-
                 }
                 else {
                     NumSlider.gridTiles[i][j].setTileStatus(false);
                 }
                 NumSlider.gridTiles[i][j].setTileValue(bases[k]);
                 imagesGroup[k].setImageBitmap(sprites[bases[k]]);
-
-                //coordinatesMap.replace(imagesViewGroup[bases[k]].getImage(), k);
-                //imagesViewGroup[k].setBitmap(sprites[bases[k]]);
-                //imagesViewGroup[k].setValue(k);
-                //imagesViewGroup[k].getImage().setImageBitmap(sprites[bases[k]]);
-
-
-
-                //imagesViewGroup[k].setValue(bases[k]);
-              //  imagesViewGroup[k].setBitmap(sprites[bases[k]]);
-               /// imagesViewGroup[k].getImage().setImageBitmap(sprites[bases[k]]);
                 k=k+1;
             }
         }
--- app/src/main/java/com/techexhange/mobileapps/assignment1/Tile.java
+++ app/src/main/java/com/techexhange/mobileapps/assignment1/Tile.java
@@ -1,5 +1,11 @@
 package com.techexhange.mobileapps.assignment1;
-// Tiles for the mobile app course
+
+/**
+ * Programmed: Lianne Sanchez Rodriguez
+ * Tile, represents the Tiles in a grid game
+ * represented as a 2D Array with properties such as
+ * value, status and activation.
+ */
 public class Tile {
 
     private int tileValue;
@@ -7,29 +13,61 @@ public class Tile {
     private boolean active;
 
 
+    /**
+     * The class constructor
+     * @param tileValue the value in the current tile.
+     * @param tileStatus the status which establishes if it is empty ot not.
+     */
     public Tile(int tileValue, boolean tileStatus){
         this.tileValue=tileValue;
         this.tileStatus=tileStatus;
         this.active=true;
     }
 
+    /**
+     * Method to access the current value of a tile.
+     * @return tileValue the Tile current value.
+     */
     public int getTileValue(){
         return this.tileValue;
     }
 
+    /**
+     * Method to change the current value of a Tile.
+     * @param newValue this is the new value that we want for our tile.
+     */
     public void setTileValue(int newValue){
         this.tileValue=newValue;
     }
 
+    /**
+     * Method to access the current status of a tile (filled or empty).
+     * @return tileStatus, which is the current status of the tile (in boolean value).
+     */
     public boolean getTileStatus(){
         return this.tileStatus;
     }
+
+    /**
+     * Method to change the current status of the Tile.
+     * @param newStatus which is the new status of the Tile.
+     */
     public void setTileStatus(boolean newStatus){
         this.tileStatus=newStatus;
     }
+
+    /**
+     * Method to change the activation method of the Tile.
+     * @param newActive , new activation value for the Tile.
+     */
     public void setActive(boolean newActive){
         this.active = false;
     }
+
+    /**
+     * Method to access the current activation value of the tile.
+     * @return active, which is the activation value of the Tile.
+     */
     public boolean getActive(){
         return this.active;
     }
